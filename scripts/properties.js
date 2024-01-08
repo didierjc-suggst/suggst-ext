bb019e3d6a176057bc14fe32d2ce3ba56ff69974
Didier Leonard-Jean Charles <didijc@gmail.com>
2024-01-08 14:18:25 -0600

    Update properties.js

--- scripts/properties.js
+++ scripts/properties.js
@@ -2,34 +2,34 @@
 
 var amd = {
     "endPoint": "https://webservices.amazon.com/onca/xml?",
-    "accessKey": "AKIAI3WQOKBGQ6OPNHRA",
-    "associateTag": "optiqly-20",
-    "secretKey": "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"
+    "accessKey": "XXXXXXX",
+    "associateTag": "XXXXXXX",
+    "secretKey": "XXXXXXX"
 };
 
 var grd = {
-    "key": "zjcfGaqWePkPoNJXN4VA",
-    "secret": "xT5cMMIceXIkGAVSbvJw16Y2sUk5g5Iy8iA6l5GcNoU"
+    "key": "XXXXXXX",
+    "secret": "XXXXXXX"
 };
 
 var fbd = {
-    "access_token": "EAAX6wBhN7bABAMQr7ULBhCzzj8h1FVApcZBMKV8tlZC0wyZBURwomt9ZA0c0o5SaxleIqPZCTuLktmv2AFyhTOx7yD1ne6HYX8mbZBTiI5gTZArTHzFqgwuWeT7c3UMaBuZBMaEtivDX21LZBaTyjGHXtt1o3jJF7KvYZD"
+    "access_token": "XXXXXXX"
 };
 
 var gle = {
-    "bookKey": "AIzaSyBCy-rBdt1tPdbE2l8CLeTFHWZZw9JtFD4",
+    "bookKey": "XXXXXXX-XXXXXXX",
     "endPoint": "https://www.googleapis.com/books/v1/volumes?q=isbn:",
     "cseEndPoint": "https://www.googleapis.com/customsearch/v1?",
-    "googleAPIkey": "AIzaSyB33xSNSTzy8ShFq21j6ZIhnNzq3a8qOAg",
-    "cx": "006105635970720037243:ktsztu1u3eg"
+    "googleAPIkey": "XXXXXXX",
+    "cx": "XXXXXXX:XXXXXXX"
 };
 
 var twt = {
-    "consumerKey": "UUVCFsK1ykdHWq4audQzniWW3",
-    "consumerSecret": "IrM129l0bAV1Jw6z6GC87l4gvcbk4qhhDPUIr23l7Vun5VcraX",
-    "accessToken": "3016005213-q49ct8SMoeANQjBQkm5r0bPutvvU0112h15Dy3q",
-    "accessTokenSecret": "zx9t29pMPYvW1Ag2gi2CP9NwG6uIKiP7BB5S8f0QQMCye",
-    "bearer": "AAAAAAAAAAAAAAAAAAAAAFYqxwAAAAAA2H4%2FoZR%2B%2FkJiWFcFYYrvpcMfM4I%3D6fIWX5XA8tZmXaAncynO5q74uCQEtSn4BOt8nvZZnhEaGf4fFa",
+    "consumerKey": "XXXXXXX",
+    "consumerSecret": "XXXXXXX",
+    "accessToken": "XXXXXXX-XXXXXXX",
+    "accessTokenSecret": "XXXXXXX",
+    "bearer": "XXXXXXX",
     "endPointSearch": "https://api.twitter.com/1.1/search/tweets.json?lang=en&result_type=recent&q=",
     "endPointMentions": "https://api.twitter.com/1.1/statuses/mentions_timeline.json?lang=en&result_type=recent&q="
 };
